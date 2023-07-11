d16390823aedb27bf250c272eec4e13c24194ae3
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-07-12 00:50:25 +0530

    TLE removed from 4sum

--- arrays/fourSum.cpp
+++ arrays/fourSum.cpp
@@ -3,29 +3,34 @@
 #include<set>
 using namespace std;
 
-vector<vector<int>> fourSum(vector<int>& nums,int target) {
-    sort(nums.begin(),nums.end());
-    set<vector<int>> s;
-    vector<vector<int>> output;
-    for(int i=0;i<nums.size()-3;i++) {
-        for(int j=i+1;j<nums.size()-2;j++) {
-            int k=j+1;
-            int l=nums.size()-1;
-            while(k<l) {
-                int sum = nums[i]+nums[j]+nums[k]+nums[l];
-                if(sum == target) {
-                    s.insert({nums[i],nums[j],nums[k],nums[l]});
-                    k++;
-                    l--;
-                } else if(sum<0) k++;
-                else l--;
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+       int n = nums.size();
+        sort(nums.begin(), nums.end());
+        set<vector<int>> set;
+        vector<vector<int>> output;
+        for(int i=0; i<n-3; i++){
+            for(int j=i+1; j<n-2; j++){
+                long long newTarget = (long long)target - (long long)nums[i] - (long long)nums[j];
+                int low = j+1, high = n-1;
+                while(low < high){
+                    if(nums[low] + nums[high] < newTarget){
+                        low++;
+                    }
+                    else if(nums[low] + nums[high] > newTarget){
+                        high--;
+                    }
+                    else{
+                        set.insert({nums[i], nums[j], nums[low], nums[high]});
+                        low++; high--;
+                    }
+                }
             }
         }
+        for(auto it : set){
+            output.push_back(it);
+        }
+        return output;
     }
-    for(auto x : s)
-        output.push_back(x);
-    return output;
-}
 
 int main() {
     vector<int> nums = {1,0,-1,0,-2,2};
