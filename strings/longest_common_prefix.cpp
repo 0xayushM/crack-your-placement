0d16613c2a701f2677ca7d652e87c06e6f4fafea
Ayush Mangal <mangal.ayush.4982@gmail.com>
2023-07-14 21:05:00 +0530

    longest common prefix

---
+++ strings/longest_common_prefix.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+string longestCommonPrefix(vector<string> &strs)
+{
+    string ans = "";
+    sort(strs.begin(), strs.end());
+    int n = strs.size();
+    string first = strs[0];
+    string last = strs[n - 1];
+    for (int i = 0; i < min(first.size(), last.size()); i++)
+    {
+        if (first[i] != last[i])
+        {
+            return ans;
+        }
+        ans += first[i];
+    }
+    return ans;
+}
+int main() {
+    vector<string> strs = {"flower","flow","flight"};
+    cout << longestCommonPrefix(strs);
+}
